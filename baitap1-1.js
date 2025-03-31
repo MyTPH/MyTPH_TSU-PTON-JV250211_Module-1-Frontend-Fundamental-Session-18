759b4519964df6bc3edab4daa046d1ff26ca2504
MyTPH <tan.phamhongmy@gmail.com>
2025-04-01 01:01:50 +0900

    Add files via upload

---
+++ baitap1-1.js
@@ -0,0 +1,10 @@
+let person={
+    name:"John Doe",
+    age:25,
+    address:"America",
+    phone:"8190345678",
+}
+console.log(person.name);
+console.log(person.age);
+console.log(person.address);
+console.log(person.phone);
---
+++ baitap1-2.js
@@ -0,0 +1,18 @@
+let student = {
+    id:1,
+    name:"Nguyễn Văn A",
+    gender:"nam",
+    age:20,
+    mark:8,
+};
+let newStudent = {
+    id:2,
+    name:"Trần Thị B",
+    gender:"nữ",
+    age:18,
+    mark:9,
+}
+let students = [];
+students.push(student);
+students.push(newStudent);
+console.log(students);
